a4c7de77b735c7b3e87ec9c996ac54a75b98773e
V1CT0R-AspIt <vict7147@edu.campusvejle.dk>
2022-06-15 13:36:50 +0200

    contactinfo tester

--- Entities/ContactInformation.cs
+++ Entities/ContactInformation.cs
@@ -4,8 +4,44 @@ namespace Entities
 {
 	public class ContactInformation
 	{
-		public int Id { get; set; }
-		public string PhoneNumber { get; set; }
-		public string Mail { get; set; }
+		private int id;
+		private string phoneNumber;
+		private string mail;
+
+		public ContactInformation(int id, string phoneNumber, string mail)
+		{
+			Id = id;
+			PhoneNumber = phoneNumber;
+			Mail = mail;
+		}
+
+		public int Id
+		{
+			get => id;
+			set
+			{
+				if(value < 0)
+				{
+					throw new ArgumentOutOfRangeException();
+				}
+				id = value;
+			}
+		}
+		public string PhoneNumber
+		{
+			get => phoneNumber;
+			set
+			{
+				phoneNumber = value;
+			}
+		}
+		public string Mail
+		{
+			get => mail;
+			set
+			{
+				mail = value;
+			}
+		}
 	}
 }
--- Tests/EntitiesTests.cs
+++ Tests/EntitiesTests.cs
@@ -8,12 +8,21 @@ using Entities;
 
 namespace Tests
 {
-	class EntitiesTests
+	public class EntitiesTests
 	{
 		[Fact]
 		public void CorrectInitialization_ContactInformation()
 		{
+			//Arrange:
+			int id = 1;
+			string mail = "";
+			string phoneNumber = "";
 
+			//Act:
+			ContactInformation contactInformation = new(id, mail, phoneNumber);
+
+			//Assert:
+			Assert.True(contactInformation.Id > 0);
 		}
 	}
 }
