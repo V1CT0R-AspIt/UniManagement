db345841743ff9afa7f1715596b9c7ea14f22608
V1CT0R-AspIt <vict7147@edu.campusvejle.dk>
2022-06-15 11:50:58 +0200

    testede vejret det er ok

---
+++ Tests/EntitiesTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Entities;
+
+namespace Tests
+{
+	class EntitiesTests
+	{
+		[Fact]
+		public void CorrectInitialization_ContactInformation()
+		{
+
+		}
+	}
+}
